4900a1eb78dda01aa8f75eaaa7eb52da4f9faf77
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-20 09:46:25 -0700

    Add tests for onload

--- cypress/integration/examples/turing-cafe_spec.cy.js
+++ cypress/integration/examples/turing-cafe_spec.cy.js
@@ -5,6 +5,30 @@ describe("main view",() => {
     it("should have a title", () => {
         cy.contains("h1","Turing Cafe Reservations")
     })
+
+    it("should have correct reservations", () => {
+        cy.contains(".resy-container", "Christie")
+        cy.contains(".resy-container", "Leta")
+        cy.contains(".resy-container", "Pam")
+    })
+
+    it("should have a background img", () => {
+        cy.get(".App").should("have.css","background-image")
+    })
+    it("should be able to fill a form", () => {
+        cy.get('input[name="name"]')
+        .type("Masaki")
+        .should("have.value","Masaki")
+        cy.get('input[name="date"]')
+        .type("12/01")
+        .should("have.value","12/01")
+        cy.get('input[name="time"]')
+        .type("4:00")
+        .should("have.value","4:00")
+        cy.get('input[name="number"]')
+        .type("14")
+        .should("have.value","14")
+    })
     it("should be able to submit a form", () => {
         cy.get('input[name="name"]')
         .type("Masaki")
